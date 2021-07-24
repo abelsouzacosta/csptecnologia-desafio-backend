795a0378cfc4e1f74ef191e77066153e1c0776fc
Abel Souza <abelsouzacosta@gmail.com>
2021-07-23 22:58:51 -0300

    feat(errors): tratamento de erros
    Adicionando a ApplicationError, responsável por lançar todos os erros da aplicação isso faz com que
    haja uma homogeneidade no tratamento de erros

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,12 +1,31 @@
 import 'reflect-metadata';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import '@shared/typeorm';
+import ApplicationError from '@shared/errors/ApplicationError';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+// tratamento de erros da aplicação
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    // verifica se error é ums instância de ApplicationError
+    if (error instanceof ApplicationError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+
+    return response.status(400).json({
+      status: 'error',
+      message: error.message,
+    });
+  },
+);
 
 app.listen(process.env.PORT);
